2cbcdc906f1f3d0008c678ba5b5500d642be6635
sstreiner <samuel.streiner@gmail.com>
2015-09-08 10:09:10 +0200

    PEP newline at EOF

--- cdotmigration/genexportpolicy.py
+++ cdotmigration/genexportpolicy.py
@@ -157,3 +157,4 @@ if __name__ == "__main__":
         printexportpolicyrules(vserver, policyname, permissions[policyname])
     for exportpolicy in exportpolicys:
         print(exportpolicys[exportpolicy])
+
